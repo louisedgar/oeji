e6d6677e6c553cb8e184b832e59655f3a7a19f07
louisedgar <louisedgar@ymail.com>
2020-02-02 17:59:45 +0700

    edit text

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,65 +4,74 @@ import homeImage from "../assets/home-img.svg";
 import Container from "@material-ui/core/Container";
 import "./Home.css";
 import Button from "@material-ui/core/Button";
-// import { createMuiTheme } from "@material-ui/core/styles";
+import { createMuiTheme } from "@material-ui/core/styles";
 import { makeStyles } from "@material-ui/core/styles";
+import MuiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
 
-// const color = createMuiTheme({
-//   palette: {
-//     primary: {
-//       light: "#62a2ff",
-//       main: "#0074e1",
-//       dark: "#004aae",
-//       contrastText: "#fff"
-//     },
-//     secondary: {
-//       light: "#ffcf49",
-//       main: "#f79e02",
-//       dark: "#be6f00",
-//       contrastText: "#000"
-//     }
-//   }
-// });
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: "#62a2ff",
+      main: "#0074e1",
+      dark: "#004aae",
+      contrastText: "#fff"
+    },
+    secondary: {
+      light: "#ffcf49",
+      main: "#f79e02",
+      dark: "#be6f00",
+      contrastText: "#000"
+    }
+  }
+});
 
 const useStyles = makeStyles(theme => ({
   root: {
-    "& > *": {
-      margin: theme.spacing(3),
-      marginLeft: 0
-    }
+    flexGrow: 1
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: "center"
   }
 }));
 
 export default function Home() {
   const classes = useStyles();
   return (
-    <Container className="home">
+    <div className={classes.root}>
       <section id="home-main">
         <Grid
           container
-          direction="row"
-          justify="center"
-          alignItems="center"
           spacing={3}
+          container
+          direction="row"
+          alignItems="center"
+          justify="center"
         >
-          <Grid>
-            <h1 className="title">Berani Oeji Dirimu?</h1>
-            <p className="subtitle">
-              Ayo uji kemampuanmu dalam kesiapan
-              <br /> menghadapi UTBK dan tingkatkan
-              <br /> pengetahuanmu disini.
-            </p>
-            <div className={classes.root}>
-              <Button size="large" variant="contained" color="primary">
-                Yuk daftar
-              </Button>
-              <Button size="large" variant="outlined" color="secondary">
-                Masuk
-              </Button>
+          <Grid item xs={8} sm={6}>
+            <div className={classes.paper}>
+              <h1 className="title">Berani Oeji Dirimu?</h1>
+              <p className="subtitle">
+                Ayo uji kemampuanmu dalam kesiapan
+                <br /> menghadapi UTBK dan tingkatkan
+                <br /> pengetahuanmu disini!
+              </p>
+              <MuiThemeProvider theme={theme}>
+                <Button size="large" variant="contained" color="primary">
+                  Yuk daftar
+                </Button>
+                <Button size="large" variant="outlined" color="secondary">
+                  Masuk
+                </Button>
+              </MuiThemeProvider>
             </div>
           </Grid>
-          <Grid className="home-img">
-            <img src={homeImage} alt="" />
+          <Grid item xs={12} sm={6}>
+            <div className={classes.paper}>
+              <div className="home-img">
+                <img src={homeImage} alt="" />
+              </div>
+            </div>
           </Grid>
         </Grid>
       </section>
@@ -77,6 +86,6 @@ export default function Home() {
           Android dan iOS mu.
         </p>
       </section>
-    </Container>
+    </div>
   );
 }
